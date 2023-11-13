12a05d22c8aedc964a62613489fab30d4b57f040
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-11-13 18:58:43 -0300

    chore: adiciona o enumerate "OrderStatus"

--- src/main/java/com/webservices/project/config/TestConfig.java
+++ src/main/java/com/webservices/project/config/TestConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import com.webservices.project.entities.Order;
 import com.webservices.project.entities.User;
+import com.webservices.project.entities.enums.OrderStatus;
 import com.webservices.project.repositories.OrderRepository;
 import com.webservices.project.repositories.UserRepository;
 
@@ -24,9 +25,11 @@ public class TestConfig implements CommandLineRunner {
     User user1 = new User(null, "User 1", "user1@email.com", "999999999", "123456");
     User user2 = new User(null, "User 2", "user2@email.com", "888888888", "abcdef");
     userRepository.saveAll(Arrays.asList(user1, user2));
-    Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), user1);
-    Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), user2);
-    Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), user1);
+    Order order1 =
+        new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.WAITING_PAYMENT, user1);
+    Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID, user2);
+    Order order3 =
+        new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.SHIPPED, user1);
     orderRepository.saveAll(Arrays.asList(order1, order2, order3));
   }
 }
--- src/main/java/com/webservices/project/entities/Order.java
+++ src/main/java/com/webservices/project/entities/Order.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.webservices.project.entities.enums.OrderStatus;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -23,6 +24,7 @@ public class Order implements Serializable {
   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",
       timezone = "GMT")
   private Instant moment;
+  private Integer orderStatus;
   @ManyToOne
   @JoinColumn(name = "client_id")
   private User client;
@@ -31,9 +33,10 @@ public class Order implements Serializable {
     super();
   }
 
-  public Order(Long id, Instant moment, User client) {
+  public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
     this.id = id;
     this.moment = moment;
+    setOrderStatus(orderStatus);
     this.client = client;
   }
 
@@ -51,6 +54,14 @@ public class Order implements Serializable {
 
   public void setMoment(Instant moment) {
     this.moment = moment;
+  }
+
+  public OrderStatus getOrderStatus() {
+    return OrderStatus.valueOf(orderStatus);
+  }
+
+  public void setOrderStatus(OrderStatus orderStatus) {
+    this.orderStatus = orderStatus.getCode();
   }
 
   public User getClient() {
